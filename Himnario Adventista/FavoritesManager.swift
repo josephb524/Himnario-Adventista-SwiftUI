9a75635b6aab7aed1eaeb61de1e2e0b5ffdb328d
Jose Pimentel <66277983+josephb524@users.noreply.github.com>
2025-10-06 19:35:43 -0400

    Add files via upload

---
+++ Himnario Adventista/FavoritesManager.swift
@@ -0,0 +1,51 @@
+//
+//  FavoritesManager.swift
+//  Himnario Adventista SwiftUI
+//
+//  Created by Jose Pimentel on 3/2/25.
+//
+
+import SwiftUI
+
+class FavoritesManager: ObservableObject {
+    @Published var favoriteHimnos: [Himnario] = [] {
+        didSet {
+            saveFavorites()
+        }
+    }
+    
+    init() {
+        loadFavorites()
+    }
+    
+    func addToFavorites(himno: Himnario) {
+        if !favoriteHimnos.contains(where: { $0.numericId == himno.numericId && $0.himnarioVersion == himno.himnarioVersion }) {
+            favoriteHimnos.append(himno)
+        }
+    }
+    
+    func removeFromFavorites(id: Int, himnarioVersion: String) {
+        if let index = favoriteHimnos.firstIndex(where: { $0.numericId == id && $0.himnarioVersion == himnarioVersion }) {
+            favoriteHimnos.remove(at: index)
+        }
+    }
+    
+    func isFavorite(id: Int, himnarioVersion: String) -> Bool {
+        return favoriteHimnos.contains(where: { $0.numericId == id && $0.himnarioVersion == himnarioVersion })
+    }
+    
+    private func saveFavorites() {
+        if let data = try? JSONEncoder().encode(favoriteHimnos) {
+            UserDefaults.standard.set(data, forKey: "favoriteHimnos")
+            // Notify PlaylistManager that favorites have changed
+            NotificationCenter.default.post(name: NSNotification.Name("FavoritesChanged"), object: nil)
+        }
+    }
+    
+    private func loadFavorites() {
+        if let data = UserDefaults.standard.data(forKey: "favoriteHimnos"),
+           let himnos = try? JSONDecoder().decode([Himnario].self, from: data) {
+            favoriteHimnos = himnos
+        }
+    }
+}
